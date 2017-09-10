10a936c4abf1485c8785cffd880902326c9bd1b6
sgindin <31820571+sgindin@users.noreply.github.com>
2017-09-10 14:51:33 +0200

    Add players.py and ignore .idea

---
+++ players.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import json
+
+russian = u"привет мир".encode('utf-8')
+print russian
+
+players = [u"иван".encode('utf-8'), u"сергей".encode('utf-8'), u"михаил".encode('utf-8'), u"владимир".encode('utf-8')]
+
+for p in players:
+    print p
+
+#print players[0:3]
+
+players = ['иван', 'Сергей', 'michail', 'vladimir']
+for p in players[0:3]:
+    print p
